62b4992b9dc7e873930a3ac1051c88ab994ee164
Brian Medley <pub-github@bmedley.org>
2015-05-24 17:58:26 -0500

    Checkpoint

--- lib/Patched/Command/patched/deploy.pm
+++ lib/Patched/Command/patched/deploy.pm
@@ -34,6 +34,7 @@ sub run {
       'api_key=s' => \my $api_key,
       'config_dbi_user=s' => \my $dbi_user,
       'config_dbi_pass=s' => \my $dbi_pass,
+      'start'    => \my $start,
       'boot'     => \my $boot;
 
     unless ($method) {
@@ -56,6 +57,10 @@ sub run {
         die("Please specify ssh or local for -method.\n");
     }
 
+    if ($start && !$boot) {
+        die("Please don't specify -start without -boot.\n");
+    }
+
     $host //= 22;
     $type //= "standalone";
 
@@ -64,8 +69,8 @@ sub run {
     my $ssh2 = Net::OpenSSH->new(host => $host, user => $user, password => $pass, port => $port, master_opts => [-o => "StrictHostKeyChecking=no"]);
 
     say("[verify supported OS] $host");
-    my $verify = $ssh2->system({timeout => 30, stdin_discard => 1, stdout_discard => 1}, "test -f /etc/centos-release && grep 'CentOS release 6' /etc/centos-release") or die("verify failed: " . $ssh2->error);
-    unless ($verify) {
+    my $system = $ssh2->system({timeout => 30, stdin_discard => 1, stdout_discard => 1}, "test -f /etc/centos-release && grep 'CentOS release 6' /etc/centos-release") or die("verify failed: " . $ssh2->error);
+    unless ($system) {
         die("We only support CentOS right now.\n");
     }
 
@@ -79,8 +84,20 @@ sub run {
         die("Install directory '$InstallDir' exists.\n");
     }
 
+    if ($boot) {
+        say("[sftp check previous boot script] $host");
+        my $not_found = 0;
+        $sftp->find("/etc/rc.d/init.d/patched", on_error => sub { $not_found = 1 });
+        unless ($not_found) {
+            die("Boot script '/etc/rc.d/init.d/patched' exists.\n");
+        }
+    }
+
     say("[sftp mkpath] $InstallDir");
-    $sftp->mkpath($InstallDir);
+    $sftp->mkpath($InstallDir) or die("sftp error: " . $sftp->error);
+
+    say("[sftp mkpath] $InstallDir/pids");
+    $sftp->mkpath("$InstallDir/pids") or die("sftp error: " . $sftp->error);
 
     my @t0 = @{[Time::HiRes::gettimeofday]};
 
@@ -106,26 +123,57 @@ sub run {
     seek($fh, 0, 0);
 
     say("[sftp put] $InstallDir/config");
-    $sftp->put($fh, "$InstallDir/config");
+    $sftp->put($fh, "$InstallDir/config") or die("sftp error: " . $sftp->error);
 
     say("[sftp put] $InstallDir/CentOS-6.6-perl-5.20.2.tar.gz");
-    $sftp->put("dist/CentOS-6.6-perl-5.20.2.tar.gz", "$InstallDir/CentOS-6.6-perl-5.20.2.tar.gz");
+    $sftp->put("dist/CentOS-6.6-perl-5.20.2.tar.gz", "$InstallDir/CentOS-6.6-perl-5.20.2.tar.gz") or die("sftp error: " . $sftp->error);
     say("[sftp symlink] $InstallDir/perl-5.20.2/bin/perl -> $InstallDir/perl");
-    $sftp->symlink("$InstallDir/perl" => "$InstallDir/perl-5.20.2/bin/perl");
+    $sftp->symlink("$InstallDir/perl" => "$InstallDir/perl-5.20.2/bin/perl") or die("sftp error: " . $sftp->error);
 
-    say("[sftp system] cd $InstallDir && tar -zxvf CentOS-6.6-perl-5.20.2.tar.gz");
-    my $install = $ssh2->system({timeout => 120, stderr_discard => 1, stdin_discard => 1, stdout_discard => 1}, "cd $InstallDir && tar -zxf CentOS-6.6-perl-5.20.2.tar.gz") or die("install failed: " . $ssh2->error);
-    unless ($install) {
+    say("[ssh2 system] cd $InstallDir && tar -zxvf CentOS-6.6-perl-5.20.2.tar.gz");
+    $system = $ssh2->system({timeout => 120, stderr_discard => 1, stdin_discard => 1, stdout_discard => 1}, "cd $InstallDir && tar -zxf CentOS-6.6-perl-5.20.2.tar.gz") or die("install failed: " . $ssh2->error);
+    unless ($system) {
         die("We only support CentOS right now.\n");
     }
 
     say("[sftp remove] $InstallDir/CentOS-6.6-perl-5.20.2.tar.gz");
-    $sftp->remove("$InstallDir/CentOS-6.6-perl-5.20.2.tar.gz");
+    $sftp->remove("$InstallDir/CentOS-6.6-perl-5.20.2.tar.gz") or die("sftp error: " . $sftp->error);
+
+    say("[sftp rput] docroot $InstallDir/docroot");
+    $sftp->rput("docroot", "$InstallDir/docroot") or die("sftp error: " . $sftp->error);
+
+    say("[sftp rput] lib $InstallDir/lib");
+    $sftp->rput("lib", "$InstallDir/lib") or die("sftp error: " . $sftp->error);
 
     unless ($boot) {
         say("[SUCCESS installed]");
         return;
     }
+
+    say("[sftp put] /etc/rc.d/init.d/patched");
+    $sftp->put("etc/CentOS-6/rc.d/init.d/patched", "/etc/rc.d/init.d/patched") or die("sftp error: " . $sftp->error);
+
+    say("[ssh2 system] chkconfig --add patched");
+    $system = $ssh2->system({timeout => 120, stderr_discard => 1, stdin_discard => 1, stdout_discard => 1}, "chkconfig --add patched") or die("install failed: " . $ssh2->error);
+    unless ($system) {
+        die("Ambiguous install: Patched may not start on boot.\n");
+    }
+
+    say("[ssh2 system] chkconfig patched on");
+    $system = $ssh2->system({timeout => 120, stderr_discard => 1, stdin_discard => 1, stdout_discard => 1}, "chkconfig patched on") or die("install failed: " . $ssh2->error);
+    unless ($system) {
+        die("Ambiguous install: Patched may not start on boot.\n");
+    }
+
+    if ($start) {
+        say("[ssh2 system] service patched start");
+        $system = $ssh2->system({timeout => 120, stderr_discard => 1, stdin_discard => 1, stdout_discard => 1}, "service patched start") or die("install failed: " . $ssh2->error);
+        unless ($system) {
+            die("Ambiguous install: Patched not started.\n");
+        }
+    }
+
+    say("[SUCCESS installed]");
 }
 
 1;
@@ -152,7 +200,9 @@ Patched::Command::patched::deploy - Deploy Patched
     --api_key <key>   Password for remote agent
     --config_dbi_user <user>   Username for DBI [used with minion]
     --config_dbi_pass <pass>   Password for DBI [used with minion]
+    --start           Start Patched after install
     --boot            Start Patched on boot
+    --start           Start Patched after install
 
 =head1 DESCRIPTION
 
